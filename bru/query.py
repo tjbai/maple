a638d88b1910c9efc36032e7e1498a9a49fa5454
TJ Bai <tjianbai@gmail.com>
2023-07-06 13:09:27 -0400

    cleaned up

--- bru/query.py
+++ bru/query.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Tuple
 
 import pinecone
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -29,7 +30,6 @@ class QueryBuilder:
         self.oai_config = oai_config
 
         pinecone.init(api_key=pc_config.api_key, environment=pc_config.env)
-
         vectorstore = Pinecone.from_existing_index(
             pc_config.index_name,
             OpenAIEmbeddings(
@@ -37,47 +37,28 @@ class QueryBuilder:
             ),
         )
 
-        """
-        # todo: look into different citation methods
-        """
-
-        """
-        # todo: can't get chat history to work...
-        self.qa = ConversationalRetrievalChain.from_llm(
+        llm = (
             ChatOpenAI(
                 openai_api_key=oai_config.api_key,
                 model_name=oai_config.completion_model,
                 temperature=0.0,
+                streaming=True,
+                callbacks=[StreamingStdOutCallbackHandler()],
             ),
-            vectorstore.as_retriever(),
-            condense_question_llm=ChatOpenAI(
-                openai_api_key=oai_config.api_key,
-                model_name=oai_config.condense_model,
-                temperature=0.0,
-            ),
-            return_source_documents=True,
-            get_chat_history=lambda h: h,
         )
+
+        """
+        # todo: look into different citation methods
+        # todo: look into conversational retrieval 
         """
 
         self.qa = RetrievalQA.from_chain_type(
-            llm=ChatOpenAI(
-                openai_api_key=oai_config.api_key,
-                model_name=oai_config.completion_model,
-                temperature=0.0,
-            ),
+            llm=llm,
             chain_type="stuff",
             retriever=vectorstore.as_retriever(),
             return_source_documents=True,
         )
 
     def query(self, q: str) -> Tuple[str, str]:
-        """
-        # todo: figure out chat history
-
-        result: Dict[Literal['question', 'answer', 'chat_history', 'source_documents'], Any]
-            = self.qa({'question': q, 'chat_history', chat_history})
-        """
-
         result = self.qa({"query": q})
         return (result["result"], result["source_documents"])
